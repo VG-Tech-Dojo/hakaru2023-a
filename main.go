5ba280b2a9b2107de7e79e2c79019f3100bd95df
Izuho Fujiwara <fujishan2212@gmail.com>
2023-09-13 17:14:18 +0900

    Bulk Insert の処理を実装

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"sync"
+	"time"
 
 	"database/sql"
 
@@ -10,15 +13,27 @@ import (
 	"os"
 )
 
-var db *sql.DB
+type Event struct {
+	Name  string
+	Value int
+	At    string
+}
+
+const batchThreshold = 100
+
+var (
+	db              *sql.DB
+	eventBuffer     []Event
+	eventBufferLock sync.Mutex
+)
 
 func init() {
-	var err error
-
 	dataSourceName := os.Getenv("HAKARU_DATASOURCENAME")
 	if dataSourceName == "" {
 		dataSourceName = "root:password@tcp(127.0.0.1:13306)/hakaru"
 	}
+
+	var err error
 
 	db, err = sql.Open("mysql", dataSourceName) // Connection Pool を作成
 	if err != nil {
@@ -39,21 +54,62 @@ func main() {
 	}
 }
 
-func hakaruHandler(w http.ResponseWriter, r *http.Request) {
-	stmt, e := db.Prepare("INSERT INTO eventlog(at, name, value) values(NOW(), ?, ?)")
-	if e != nil {
-		panic(e.Error())
+/**
+ * eventBuffer が閾値を超えたら Bulk Insert する
+ * @query INSERT INTO eventlog(at, name, value) VALUES ('', '', ''), ('', '', ''), ('', '', '')
+ */
+func bulkInsert() {
+	eventBufferLock.Lock()
+	defer eventBufferLock.Unlock()
+
+	if (len(eventBuffer)) == 0 {
+		return
 	}
 
-	defer stmt.Close()
+	log.Printf("[BULK] Start: %d", len(eventBuffer))
 
-	name := r.URL.Query().Get("name")
-	value := r.URL.Query().Get("value")
+	query := "INSERT INTO eventlog(at, name, value) VALUES"
+	values := []interface{}{}
 
-	_, err := stmt.Exec(name, value)
+	for _, event := range eventBuffer {
+		query += "(?, ?, ?),"
+		values = append(values, event.At, event.Name, event.Value)
+	}
+	query = query[:len(query)-1] // 最後のカンマを削除
+
+	// 直接 SQL を実行しているが動的プリペアドステートメントのため SQL インジェクションの心配はない
+	_, err := db.Exec(query, values...)
 	if err != nil {
 		panic(err.Error())
 	}
+
+	eventBuffer = eventBuffer[:0] // バッファーをクリア
+
+	log.Printf("[BULK] End: %d", len(eventBuffer))
+}
+
+func hakaruHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	value := r.URL.Query().Get("value")
+
+	if value == "" {
+		value = "1"
+	}
+
+	valueInt, err := strconv.Atoi(value)
+	if err != nil {
+		return
+	}
+
+	at := time.Now().Format("2006-01-02 15:04:05")
+
+	eventBufferLock.Lock()
+	eventBuffer = append(eventBuffer, Event{Name: name, Value: valueInt, At: at})
+	if (len(eventBuffer)) >= batchThreshold {
+		log.Printf("[HAKARU] Bulk Triggered: %d", len(eventBuffer))
+		go bulkInsert()
+	}
+	eventBufferLock.Unlock()
 
 	origin := r.Header.Get("Origin")
 	if origin != "" {
