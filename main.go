b9152c13b84150fdd36ab05f513cdd24dfe33a8c
Yajima Soichi <jajima.jp@gmail.com>
2023-09-13 16:42:25 +0900

    Statsの追加

--- main.go
+++ main.go
@@ -6,8 +6,9 @@ import (
 
 	"database/sql"
 
-	_ "github.com/go-sql-driver/mysql"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
@@ -49,6 +50,12 @@ func hakaruHandler(w http.ResponseWriter, r *http.Request) {
 
 	name := r.URL.Query().Get("name")
 	value := r.URL.Query().Get("value")
+
+	stats := db.Stats()
+
+	waitDuration := stats.WaitDuration
+	log.Printf("waitDuration: ")
+	log.Print(waitDuration)
 
 	_, err := stmt.Exec(name, value)
 	if err != nil {
