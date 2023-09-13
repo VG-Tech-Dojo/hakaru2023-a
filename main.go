ac90857a9eb7d152cc853e41bc8c93194991ada1
Yajima Soichi <jajima.jp@gmail.com>
2023-09-13 16:54:30 +0900

    Show WaitCount

--- main.go
+++ main.go
@@ -53,9 +53,9 @@ func hakaruHandler(w http.ResponseWriter, r *http.Request) {
 
 	stats := db.Stats()
 
+	waitCount := stats.WaitCount
 	waitDuration := stats.WaitDuration
-	log.Printf("waitDuration: ")
-	log.Print(waitDuration)
+	log.Printf("waitCount: %d, waitDuration: %v", waitCount, waitDuration)
 
 	_, err := stmt.Exec(name, value)
 	if err != nil {
